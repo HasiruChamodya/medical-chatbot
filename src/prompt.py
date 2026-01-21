84a4b53a2edf5257f2abca623540df66c21bedbc
HasiruChamodya <commenty460@gmail.com>
2026-01-21 18:22:29 +0530

    .html .css are done and app.py complete

---
+++ src/prompt.py
@@ -0,0 +1,7 @@
+system_prompt = (
+    "You a helpful medical assistant. Use the following context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "Use three sentences at most to answer the question and keep it concise."
+    "\n\n"
+    "{context}"
+)
